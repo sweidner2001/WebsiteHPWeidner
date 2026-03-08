00c4f9afc535294c9e3713af9bfa757afe77db74
Sebastian Weidner <sweidner.2001@gmail.com>
2026-03-08 20:09:13 +0100

    chg: Layout draft 3

--- src/app/components/services/Services_3.tsx
+++ src/app/components/services/Services_3.tsx
@@ -55,7 +55,8 @@ const services: IService[] = [
         icon: (<svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>),
         theme: 'dark',
         // col 5–8 (4 von 12), row 5–6
-        grid: 'md:col-start-5 md:col-span-4 md:row-start-5 md:row-span-2 md:self-start',
+        // grid: 'md:col-start-5 md:col-span-4 md:row-start-5 md:row-span-2 md:self-start',
+        grid: 'md:col-start-4 md:col-span-6 md:row-start-5 md:row-span-2 md:self-start',
         size: 'medium',
     },
 
@@ -84,7 +85,8 @@ const services: IService[] = [
         icon: (<svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" /></svg>),
         theme: 'light',
         // col 5–7 (3 von 12), row 4
-        grid: 'md:col-start-9 md:col-span-3 md:row-start-5 md:row-span-1',
+        // grid: 'md:col-start-9 md:col-span-3 md:row-start-5 md:row-span-1',
+        grid: 'md:col-start-10 md:col-span-3 md:row-start-5 md:row-span-1',
         size: 'small',
     },
     {
@@ -93,7 +95,8 @@ const services: IService[] = [
         icon: (<svg className="w-7 h-7" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>),
         theme: 'earth',
         // col 1–3 (3 von 12), row 6
-        grid: 'md:col-start-2 md:col-span-3 md:row-start-5 md:row-span-2 md:self-start',
+        // grid: 'md:col-start-2 md:col-span-3 md:row-start-5 md:row-span-2 md:self-start',
+        grid: 'md:col-start-1 md:col-span-3 md:row-start-5 md:row-span-2 md:self-start',
         size: 'small',
     },
 ];
