efcbe5e586ec26ee0e26abfb3a2e9460a3fc70fc
Sebastian Weidner <sweidner.2001@gmail.com>
2026-04-18 16:46:49 +0200

    Mod. text

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer className="bg-[#0d9c42] py-8 border-t border-green-600 mt-[1px]">
             <div className="container mx-auto px-6 text-center">
-                <p className="text-white font-light">Naturheilpraxis Carola Weidner</p>
+                <p className="text-white font-light">Naturheilpraxis Carola Weidner TEST</p>
                 <div className="mt-2">
                     <Link href="/datenschutz"
                           className="text-white hover:text-green-200 text-sm font-light mx-2">Datenschutz</Link>
