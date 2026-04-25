2e0d4abc3d3094201ff89310da5d53398240bd47
Sebastian Weidner <sweidner.2001@gmail.com>
2026-04-25 11:24:44 +0200

    Add: Files for Tab-Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,13 @@ export const metadata: Metadata = {
     locale: "de_DE",
     type: "website",
   },
+  icons: {
+    // icon: [
+    //   { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    //   { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    // ],
+    // apple: "/favicon_io/apple-touch-icon.png",
+  },
   robots: {
     index: true,
     follow: true,
